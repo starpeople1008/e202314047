b8c7daa12a8ddc09a4c4aee6867fe16c5dad168a
Student A <a@example.com>
2025-09-17 17:19:36 +0900

    six

--- Main.java
+++ Main.java
@@ -1,2 +1,3 @@
-public class Main { public static void main(String[] args) { System.out.println("조성민"); }
-{ System.out.println("안녕하세요"); } }
+public class Main { public static void main(String[] args) { System.out.println("조성민"); 
+System.out.println("안녕하세요");
+System.out.println("hello world"); }  }
