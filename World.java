812bd9fc8b8d5425e78f58d87e42ab26168b475a
Student A <a@example.com>
2025-09-17 17:13:01 +0900

    four

--- World.java
+++ World.java
@@ -1,1 +1,2 @@
-public class World { public static void run() { System.out.println("202314047"); } }
+public class World { public static void run() { System.out.println("202314047"); }
+{ System.out.println("hello world"); }}
